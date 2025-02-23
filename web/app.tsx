c5c0709425fa53e4252b4749e7e4b08f4601146f
sawka <mike@commandline.dev>
2025-02-23 12:00:04 -0800

    jotai

--- web/app.tsx
+++ web/app.tsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./app.css";
+import { atom, useAtom } from "jotai";
+
+const countAtom = atom(0);
+countAtom.debugLabel = "countAtom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useAtom(countAtom);
 
   return (
     <>
