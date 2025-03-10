a1e58d2ab6870db5b8a5f502c6350191817aceb8
sawka <mike@commandline.dev>
2025-03-10 14:13:57 -0700

    add keyutil

--- web/types/custom.d.ts
+++ web/types/custom.d.ts
@@ -9,6 +9,34 @@ declare global {
     interface AbstractRpcClient {
         recvRpcMessage(msg: RpcMessage): void;
     }
+
+    // vdom.WaveKeyboardEvent
+    type OutrigKeyboardEvent = {
+        type: "keydown" | "keyup" | "keypress" | "unknown";
+        key: string;
+        code: string;
+        repeat?: boolean;
+        location?: number;
+        shift?: boolean;
+        control?: boolean;
+        alt?: boolean;
+        meta?: boolean;
+        cmd?: boolean;
+        option?: boolean;
+    };
+
+    type KeyPressDecl = {
+        mods: {
+            Cmd?: boolean;
+            Option?: boolean;
+            Shift?: boolean;
+            Ctrl?: boolean;
+            Alt?: boolean;
+            Meta?: boolean;
+        };
+        key: string;
+        keyType: string;
+    };
 }
 
 export {};
