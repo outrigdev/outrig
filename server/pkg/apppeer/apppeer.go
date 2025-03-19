9d22119cf8f0288413dea8d1a24b3ab90af4e991
sawka <mike@commandline.dev>
2025-03-18 17:35:45 -0700

    remove log writer

--- server/pkg/apppeer/apppeer.go
+++ server/pkg/apppeer/apppeer.go
@@ -16,7 +16,6 @@ import (
 
 	"github.com/outrigdev/outrig"
 	"github.com/outrigdev/outrig/pkg/ds"
-	"github.com/outrigdev/outrig/pkg/logwriter"
 	"github.com/outrigdev/outrig/pkg/rpctypes"
 	"github.com/outrigdev/outrig/pkg/utilds"
 	"github.com/outrigdev/outrig/pkg/utilfn"
@@ -48,7 +47,6 @@ type AppRunPeer struct {
 	Status           string                              // Current status of the application
 	LastModTime      int64                               // Last modification time in milliseconds
 	LineNum          atomic.Int64                        // Atomic counter for log line numbers
-	LogWriter        *logwriter.LogWriter                // Buffered log writer for persistent storage
 	refCount         int                                 // Reference counter
 	refLock          sync.Mutex                          // Lock for reference counter operations
 }
@@ -100,7 +98,6 @@ func GetAppRunPeer(appRunId string) *AppRunPeer {
 			RuntimeStats:  utilds.MakeCirBuf[ds.RuntimeStatsInfo](RuntimeStatsBufferSize),
 			Status:        AppStatusRunning,
 			LastModTime:   time.Now().UnixMilli(),
-			LogWriter:     nil,
 			refCount:      0,
 		}
 	})
@@ -108,25 +105,7 @@ func GetAppRunPeer(appRunId string) *AppRunPeer {
 	// Increment reference counter
 	peer.refLock.Lock()
 	defer peer.refLock.Unlock()
-
-	// Create log writer when reference count goes from 0 to 1
-	if peer.refCount == 0 && peer.LogWriter == nil {
-		// Create a directory for this app run
-		appRunDir := getAppRunDir(appRunId)
-
-		// Create log writer
-		logFilePath := filepath.Join(appRunDir, "logs.log")
-		logWriter, err := logwriter.MakeLogWriter(logFilePath)
-		if err != nil {
-			log.Printf("Failed to create log writer for app run %s: %v", appRunId, err)
-		} else {
-			peer.LogWriter = logWriter
-			log.Printf("Created log writer for app run %s", appRunId)
-		}
-	}
-
 	peer.refCount++
-	log.Printf("Incremented reference count for app run %s to %d", appRunId, peer.refCount)
 
 	return peer
 }
@@ -137,7 +116,6 @@ func (p *AppRunPeer) Release() {
 	defer p.refLock.Unlock()
 
 	p.refCount--
-	log.Printf("Decremented reference count for app run %s to %d", p.AppRunId, p.refCount)
 
 	if p.refCount > 0 {
 		return
@@ -147,14 +125,6 @@ func (p *AppRunPeer) Release() {
 		p.Status = AppStatusDisconnected
 		p.LastModTime = time.Now().UnixMilli()
 		log.Printf("Connection closed for app run ID: %s, marked as disconnected", p.AppRunId)
-	}
-
-	// Dispose the log writer
-	if p.LogWriter != nil {
-		if err := p.LogWriter.Dispose(); err != nil {
-			log.Printf("Error disposing log writer for app run %s: %v", p.AppRunId, err)
-		}
-		p.LogWriter = nil
 	}
 }
 
@@ -229,11 +199,6 @@ func (p *AppRunPeer) HandlePacket(packetType string, packetData json.RawMessage)
 
 		// Add log line to circular buffer
 		p.Logs.Write(logLine)
-
-		// Write to persistent log file using the buffered log writer
-		if p.LogWriter != nil {
-			p.LogWriter.WriteLogLine(&logLine)
-		}
 
 	case ds.PacketTypeGoroutine:
 		var goroutineInfo ds.GoroutineInfo
