fbba03d4c16d0df1dc110e74114e704d513b3746
sawka <mike@commandline.dev>
2025-03-18 22:04:10 -0700

    implement "outrig run"

--- server/main-server.go
+++ server/main-server.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/outrigdev/outrig/server/pkg/boot"
 	"github.com/outrigdev/outrig/server/pkg/serverbase"
@@ -15,10 +17,51 @@ var OutrigVersion = "v0.0.0"
 // OutrigBuildTime is the build timestamp of Outrig
 var OutrigBuildTime = ""
 
+// shouldRunGoCommand checks if the command line arguments indicate we should run the go command
+func shouldRunGoCommand() bool {
+	// Look for the first argument that doesn't start with a "-"
+	for i := 1; i < len(os.Args); i++ {
+		if !strings.HasPrefix(os.Args[i], "-") {
+			// If it's "run", we should run the go command
+			return os.Args[i] == "run"
+		}
+	}
+	return false
+}
+
+// runGoCommand executes a go command with the provided arguments
+func runGoCommand() error {
+	// Replace "outrig" with "go" and execute
+	execArgs := make([]string, len(os.Args))
+	execArgs[0] = "go"
+	copy(execArgs[1:], os.Args[1:])
+	
+	execCmd := exec.Command(execArgs[0], execArgs[1:]...)
+	execCmd.Stdout = os.Stdout
+	execCmd.Stderr = os.Stderr
+	execCmd.Stdin = os.Stdin
+	
+	err := execCmd.Run()
+	if exitErr, ok := err.(*exec.ExitError); ok {
+		os.Exit(exitErr.ExitCode())
+	}
+	return err
+}
+
 func main() {
 	// Set serverbase version from main version (which gets overridden by build tags)
 	serverbase.OutrigVersion = OutrigVersion
 	serverbase.OutrigBuildTime = OutrigBuildTime
+
+	// Check if we should run the go command
+	if shouldRunGoCommand() {
+		err := runGoCommand()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error executing command: %v\n", err)
+			os.Exit(1)
+		}
+		return
+	}
 
 	// Create the root command
 	rootCmd := &cobra.Command{
