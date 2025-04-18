531af257b327e3e8c043b190cff85b57e78a94cf
sawka <mike@commandline.dev>
2025-04-17 22:04:45 -0700

    bump to v0.2.3-alpha

--- pkg/base/base.go
+++ pkg/base/base.go
@@ -14,7 +14,7 @@ const DefaultDomainSocketName = "/outrig.sock"
 const ExternalLogCaptureEnvName = "OUTRIG_EXTERNALLOGCAPTURE"
 const AppRunIdEnvName = "OUTRIG_APPRUNID"
 
-const OutrigSDKVersion = "v0.2.1"
+const OutrigSDKVersion = "v0.2.2-alpha"
 
 // Client-specific functions that use the client's Dev flag
 
