ef26fcfb74afd84a907af2a31cd09a369dd205e0
sawka <mike@commandline.dev>
2025-04-25 12:35:31 -0700

    update to v0.3.3

--- pkg/base/base.go
+++ pkg/base/base.go
@@ -14,7 +14,7 @@ const DefaultDomainSocketName = "/outrig.sock"
 const ExternalLogCaptureEnvName = "OUTRIG_EXTERNALLOGCAPTURE"
 const AppRunIdEnvName = "OUTRIG_APPRUNID"
 
-const OutrigSDKVersion = "v0.3.2-alpha"
+const OutrigSDKVersion = "v0.3.3"
 
 // Client-specific functions that use the client's Dev flag
 
