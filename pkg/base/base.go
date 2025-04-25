22fe614aebdb76cc93ea38b930d2895797a6f42f
sawka <mike@commandline.dev>
2025-04-25 12:44:50 -0700

    Bump version to v0.3.4

--- pkg/base/base.go
+++ pkg/base/base.go
@@ -14,7 +14,7 @@ const DefaultDomainSocketName = "/outrig.sock"
 const ExternalLogCaptureEnvName = "OUTRIG_EXTERNALLOGCAPTURE"
 const AppRunIdEnvName = "OUTRIG_APPRUNID"
 
-const OutrigSDKVersion = "v0.3.3"
+const OutrigSDKVersion = "v0.3.4"
 
 // Client-specific functions that use the client's Dev flag
 
--- server/pkg/serverbase/serverbase.go
+++ server/pkg/serverbase/serverbase.go
@@ -16,7 +16,7 @@ import (
 
 // OutrigServerVersion is the current version of Outrig
 // This gets set from main-server.go during initialization
-var OutrigServerVersion = "v0.3.3"
+var OutrigServerVersion = "v0.3.4"
 
 // OutrigBuildTime is the build timestamp of Outrig
 // This gets set from main-server.go during initialization
