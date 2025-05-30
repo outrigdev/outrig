2eb87ef1204b404af2f4b925c326dff35e54748f
sawka <mike@commandline.dev>
2025-05-29 20:42:55 -0700

    Bump version to v0.6.3

--- macosapp/main-outrigapp.go
+++ macosapp/main-outrigapp.go
@@ -28,7 +28,7 @@ import (
 
 var (
 	// Version information
-	OutrigAppVersion = "v0.6.2"
+	OutrigAppVersion = "v0.6.3"
 
 	// Server process
 	serverCmd          *exec.Cmd
--- server/pkg/serverbase/serverbase.go
+++ server/pkg/serverbase/serverbase.go
@@ -16,7 +16,7 @@ import (
 
 // OutrigServerVersion is the current version of Outrig
 // This gets set from main-server.go during initialization
-var OutrigServerVersion = "v0.6.2"
+var OutrigServerVersion = "v0.6.3"
 
 // OutrigBuildTime is the build timestamp of Outrig
 // This gets set from main-server.go during initialization
