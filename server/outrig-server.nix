69a1a85d579090dc928b3aff4fcc22023dd0d400
sawka <mike@commandline.dev>
2025-06-20 10:40:20 -0700

    update hashes for all 4 supported platforms

--- server/outrig-server.nix
+++ server/outrig-server.nix
@@ -16,6 +16,14 @@ let
       "Darwin"
     else
       platform;
+
+  # Platform-specific hashes for v0.8.2
+  hashes = {
+    "x86_64-linux" = "sha256:a794bd7788123baf7b842ac9bfe2f8ea9d2dbdcbe5399792562efc3c66cba06c";
+    "aarch64-linux" = "sha256:dd4960b8b272c99bd4a6b30f85e3dd99146c5ab3457ecbfda26dac75cad80ab5";
+    "x86_64-darwin" = "sha256:356b01f184ebcc0862eb0b46d1241a36b23f2e67bc51bc30eb3a1186e145c23e";
+    "aarch64-darwin" = "sha256:b723a6b79650ba201d19d5fa70871d43c6095bc6d383502d989a2f83c8d7bb0d";
+  };
 in
 (stdenv.mkDerivation rec {
   pname = "outrig";
@@ -23,7 +31,7 @@ in
 
   src = fetchzip {
     url = "https://github.com/outrigdev/outrig/releases/download/v${version}/${pname}_${version}_${platform}_${arch}.tar.gz";
-    hash = "sha256-0oJmBJCRCvY2ByFA7MosQ0y+065KvnUJNTczVRfqcF4=";
+    hash = hashes.${stdenv.hostPlatform.system};
   };
 
   dontBuild = true;
