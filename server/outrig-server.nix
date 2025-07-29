851d9d10ca00e860db6846cb25fea02c9e75d32f
Mike Sawka <mike@commandline.dev>
2025-07-28 17:44:16 -0700

    Update Nix package for v0.9.0 release (#203)
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>

--- server/outrig-server.nix
+++ server/outrig-server.nix
@@ -17,17 +17,17 @@ let
     else
       platform;
 
-  # Platform-specific hashes for v0.8.3
+  # Platform-specific hashes for v0.9.0
   hashes = {
-    "x86_64-linux" = "0b76d17ff1c22cafde664fde95337db804b245fc36c0c151bad2bf65d8613c61";
-    "aarch64-linux" = "f0b9b312913828f5c0b16839e8c0fe3c62a1720dd5f70712d7140a7ff759c67b";
-    "x86_64-darwin" = "68799bcaa3b5d04b1c1d4ad39c22b37a7cf28ebeb0edde20495587dec2b4442d";
-    "aarch64-darwin" = "6061b9e9cc4288c77190fe26248877ac8079fe9ae03d8b92c4ace25302d34ec8";
+    "x86_64-linux" = "d2eeaa8cf3d8b64b57b5638f2df8219c38c8f10063765f8e9b045bc22c355781";
+    "aarch64-linux" = "7e5f7f28bbbcad069673aae6f93a14083f61969533441f8feb23c12ede90fff8";
+    "x86_64-darwin" = "97d474f7f4e371b6986f69c6fa09a43a75e4b9f22939a93512b9ce8a38d3d780";
+    "aarch64-darwin" = "053818a443b91cc93840f28f0d7d92603e0f73f184d7fd15499c7536759a98fd";
   };
 in
 (stdenv.mkDerivation rec {
   pname = "outrig";
-  version = "0.8.3";
+  version = "0.9.0";
 
   src = fetchurl {
     url = "https://github.com/outrigdev/outrig/releases/download/v${version}/${pname}_${version}_${platform}_${arch}.tar.gz";
